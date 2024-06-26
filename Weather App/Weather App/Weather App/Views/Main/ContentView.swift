a607984b3495ccacf620cd62957d7172a4eab2a9
ArturMigulea <114543708+ArturMigulea@users.noreply.github.com>
2024-06-26 18:58:49 -0400

    Add files via upload

---
+++ Weather App/Weather App/Weather App/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Horst WaffleTon on 2024-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather App/Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Horst WaffleTon on 2024-06-25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
